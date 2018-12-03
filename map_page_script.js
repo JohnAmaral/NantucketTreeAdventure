c44eb7d9316e6a86fa940854c011a4340d89de31
John Amaral <john3r8amaral@yahoo.com>
2018-12-03 15:35:01 -0500

    Start tracking automatically, zoom, cleaned up code

--- map_page_script.js
+++ map_page_script.js
@@ -1,8 +1,3 @@
 // map_page_script.js
 // JavaScript file for map page
 // Mainly for tracking user location
-
-function getLocation() {
-    $.get("http://maps.googleapis.com/maps/api/geocode/json?latlng="+
-    position.coords.latitude + "," + position.coords.longitude +"&sensor=false")
-}
